dfa6a70c12e50b739649fa8a2a86c5b0d2ac9db0
F1eshka <144143888+F1eshka@users.noreply.github.com>
2023-11-03 14:33:17 +0300

    Add files via upload

---
+++ HW04.11.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int range;
+
+	int* pa=&a;
+	int* pb=&b;
+	cin>>*pa;
+	cin>>*pb;
+
+	int* prange=&range;
+	*prange=pb-pa;
+
+	cout<<"Distance a of variables: "<< *prange <<"\n";
+
+	cout<<"Distance in bytes: "<<sizeof(int) * *prange <<"\n";
+}
